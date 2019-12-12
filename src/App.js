5a77563221a01fc420843422dbe6a716957908cf
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-11 22:26:11 -0500

    working on typing animation for subtitle

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
       <div className="left-div"></div>
       <div className="name-title-container">
         <h1 className="name-header">Tyler Eikenberg</h1>
+        <div className="subname-container">
+          <h3>Software Engineer</h3>
+        </div>
+        <i class="fab fa-github-square"></i>
       </div>
       <div className="right-div"></div>
     </body>
