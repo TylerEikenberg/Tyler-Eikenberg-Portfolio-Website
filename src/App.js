565ced143e5dba0368685e41e567bfbeebfaf1ba
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-12 19:12:05 -0500

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from "react";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Projects from "./Pages/Projects";
 
 function App() {
   return (
-    <body className="wrapper">
-      <div className="left-div"></div>
-      <div className="name-title-container">
-        <h1 className="name-header">Tyler Eikenberg</h1>
-        <div className="subname-container">
-          <h3>Software Engineer</h3>
-        </div>
-        <i class="fab fa-github-square"></i>
-      </div>
-      <div className="right-div"></div>
-    </body>
+    <Router>
+      <body className="wrapper">
+        <Route path="/" component={Home} />
+        <Route path="/projects" component={Projects} />
+      </body>
+    </Router>
   );
 }
+
+const Home = () => (
+  <body className="wrapper">
+    <div className="left-div"></div>
+    <div className="name-title-container">
+      <h1 className="name-header">Tyler Eikenberg</h1>
+      <div className="subname-container">
+        <h3>Software Engineer</h3>
+      </div>
+      <i class="fab fa-github-square"></i>
+    </div>
+
+    <div className="right-div"></div>
+  </body>
+);
 
 export default App;
---
+++ src/Pages/Projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <body className="wrapper">
+      <div>hello</div>
+    </body>
+  );
+}
+
+export default Projects;
