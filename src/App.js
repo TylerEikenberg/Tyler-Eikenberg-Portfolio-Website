150116001bf2dc81744d438b23e6973a53ebef91
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-12 21:42:15 -0500

    adding icons

--- src/App.js
+++ src/App.js
@@ -2,38 +2,42 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Projects from "./Pages/Projects";
-import { mdiGithubBox } from "@mdi/js";
-import { mdiLinkedinBox } from "@mdi/js";
+import { mdiGithubBox, mdiLinkedinBox, mdiEmailPlus } from "@mdi/js";
 import Icon from "@mdi/react";
 
 function App() {
   return (
     <Router>
-      <body className="wrapper">
+      <div className="wrapper">
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/projects" exact component={Projects} />
         </Switch>
-      </body>
+      </div>
     </Router>
   );
 }
 
 const Home = () => (
-  <body className="wrapper">
+  <div className="wrapper">
     <div className="left-div">
       <div className="subname-container">
         <h3>Software Engineer</h3>
         <div className="nav-links">
           <ul>
-            <li>
+            <li className="icon-link">
               <a href="https://github.com/TylerEikenberg">
                 <Icon path={mdiGithubBox} size={3} color="#4F759B" />
               </a>
             </li>
-            <li>
+            <li className="icon-link">
               <a href="https://www.linkedin.com/in/tylereikenberg/">
                 <Icon path={mdiLinkedinBox} size={3} color="#4F759B" />
+              </a>
+            </li>
+            <li className="icon-link">
+              <a href="https://www.linkedin.com/in/tylereikenberg/">
+                <Icon path={mdiEmailPlus} size={3} color="#4F759B" />
               </a>
             </li>
           </ul>
@@ -45,7 +49,7 @@ const Home = () => (
     </div>
 
     <div className="right-div"></div>
-  </body>
+  </div>
 );
 
 export default App;
