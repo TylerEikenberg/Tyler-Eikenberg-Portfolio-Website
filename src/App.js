86ca6d2842332f9edb0c0f5f738670318328db9a
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-12 20:29:59 -0500

    adjusting postioning for mobile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Projects from "./Pages/Projects";
+import { mdiGithubBox } from "@mdi/js";
+import { mdiLinkedinBox } from "@mdi/js";
+import Icon from "@mdi/react";
 
 function App() {
   return (
@@ -21,6 +24,20 @@ const Home = () => (
     <div className="left-div">
       <div className="subname-container">
         <h3>Software Engineer</h3>
+        <div className="nav-links">
+          <ul>
+            <li>
+              <a href="https://github.com/TylerEikenberg">
+                <Icon path={mdiGithubBox} size={3} color="#4F759B" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/tylereikenberg/">
+                <Icon path={mdiLinkedinBox} size={3} color="#4F759B" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
     <div className="name-title-container">
