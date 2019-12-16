94c06f6ebd526db29478a6ca5eca0c81b7fa4f2e
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-15 21:34:49 -0500

    added three projects

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -4,6 +4,7 @@ import "./Projects.css";
 import Project from "../Components/Project/Project";
 import placeholder from "../Assets/cotripper-ph.png";
 import delicious from "../Assets/delicious.jpg";
+import gdawg from "../Assets/gdawg.jpg";
 
 function Projects() {
   return (
@@ -21,16 +22,25 @@ function Projects() {
         </h2>
         <span></span>
       </div>
+
       <div className="projects-body">
         <span></span>
         <div className="placeholder-project">
           <Project picture={placeholder}>
             <h1>CoTripper</h1>
             <h2>
-              As part of a team of 25 other developers, I helped create a
-              prototype for CoTripper, an application for mothers to get
-              together and plan trips for themselves and their kids. I learned
-              so much from this project, and I loved working with a team.
+              Built with React, Python, and Django. As part of a team of 25
+              other developers, I helped create a prototype for CoTripper, an
+              application for mothers to get together and plan trips for
+              themselves and their kids. I learned so much from this project,
+              and I loved working with a team.{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/CotripperPlatform/CoTrip"
+              >
+                Take a look at the repo on GitHub.
+              </a>
             </h2>
           </Project>
         </div>
@@ -71,6 +81,37 @@ function Projects() {
                 back-end
               </a>{" "}
               repos on GitHub.
+            </h2>
+          </Project>
+        </div>
+        <span></span>
+      </div>
+
+      <div className="projects-body">
+        <span></span>
+        <div className="placeholder-project">
+          <Project picture={gdawg}>
+            <h1>Galaxy Dawg</h1>
+            <h2>
+              Galaxy Dawg is a game I made in a few days using JavaScript and
+              Phaser JS. This was a lot of fun to work on and I learned a lot
+              while making it (mostly that designing games is really hard!).{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://tylereikenberg.github.io/Galaxy-Dawg/"
+              >
+                Play it here!
+              </a>{" "}
+              And check out the repo{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/TylerEikenberg/Galaxy-Dawg"
+              >
+                here on GitHub
+              </a>
+              .
             </h2>
           </Project>
         </div>
