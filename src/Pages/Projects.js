00bf3e764c7e3688372a6bbc44fb3b0a95718145
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-15 19:19:47 -0500

    Adding to project page

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -10,6 +10,14 @@ function Projects() {
         <h1>Projects</h1>
         <span></span>
       </div>
+      <div className="project-preamble">
+        <span></span>
+        <h2>
+          Here's a few of my most recent projects. For a closer look be sure to
+          check out my <a href="/">GitHub</a>.
+        </h2>
+        <span></span>
+      </div>
     </div>
   );
 }
